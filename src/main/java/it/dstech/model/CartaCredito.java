aca28a1a0eedc8d10f952eecbcaf988daf3d0171
simone <s.sorcinelli@gmail.com>
2017-12-11 12:57:42 +0100

    creata classe carta di credito

---
+++ src/main/java/it/dstech/model/CartaCredito.java
@@ -0,0 +1,122 @@
+package it.dstech.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class CartaCredito {
+	
+	private int id;
+	
+	private String numero;
+	
+	private LocalDate scadenza;
+	
+	private String ccv;
+	
+	private double credito;
+	
+	@ManyToOne
+	@JoinColumn(name="USER_ID")
+	private User user;
+	
+	public CartaCredito() {
+		
+	}
+
+	public CartaCredito(int id, String numero, LocalDate scadenza, String ccv, double credito, User user) {
+		this.id = id;
+		this.numero = numero;
+		this.scadenza = scadenza;
+		this.ccv = ccv;
+		this.credito = credito;
+		this.user = user;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getNumero() {
+		return numero;
+	}
+
+
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+
+
+	public LocalDate getScadenza() {
+		return scadenza;
+	}
+
+
+
+	public void setScadenza(LocalDate scadenza) {
+		this.scadenza = scadenza;
+	}
+
+
+
+	public String getCcv() {
+		return ccv;
+	}
+
+
+
+	public void setCcv(String ccv) {
+		this.ccv = ccv;
+	}
+
+
+
+	public double getCredito() {
+		return credito;
+	}
+
+
+
+	public void setCredito(double credito) {
+		this.credito = credito;
+	}
+
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "CartaCredito [id=" + id + ", numero=" + numero + ", scadenza=" + scadenza + ", ccv=" + ccv
+				+ ", credito=" + credito + ", user=" + user + "]";
+	}
+	
+	
+	
+
+}
