c9478f298c0f46dd64ad7b7a7ae47d3ff0cf70fa
kirio <kirio@192.168.236.163>
2017-12-11 13:35:24 +0100

    aggiornata classe User

--- src/main/java/it/dstech/model/User.java
+++ src/main/java/it/dstech/model/User.java
@@ -1,11 +1,20 @@
 package it.dstech.model;
 
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class User {
@@ -17,11 +26,55 @@ public class User {
 	private String username;
 	
 	private String password;
-	
 	@Enumerated(EnumType.STRING)
+	
 	private UserProfileType profileType;
+	@JsonIgnore
+	@ManyToMany
+	@JoinTable(
+		      name="USER_PRODJ",
+		      joinColumns= @JoinColumn (name="USERJ_ID", referencedColumnName="ID"),
+		      inverseJoinColumns=@JoinColumn(name="PRODJ_ID", referencedColumnName="ID")
+		      )
+	private TipoUtente logtype;
+	@JsonIgnore
+	@ManyToMany
+	@JoinTable(
+		      name="USER_PRODJ",
+		      joinColumns= @JoinColumn (name="USERJ_ID", referencedColumnName="ID"),
+		      inverseJoinColumns=@JoinColumn(name="PRODJ_ID", referencedColumnName="ID")
+		      )
+	
+	private String tel;
+	
+	private String via;
+	
+	private String cap;
+	
+	private String citta;
+	
+	private String prov;
+	
+	@OneToMany(fetch=FetchType.LAZY, mappedBy="user")
+	private List<CartaCredito> listaCartaCredito;
 	
 	public User() {
+		
+	}
+
+	public User(int id, String username, String password, UserProfileType profileType, TipoUtente logtype, String tel,
+			String via, String cap, String citta, String prov, List<CartaCredito> listaCartaCredito) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.profileType = profileType;
+		this.logtype = logtype;
+		this.tel = tel;
+		this.via = via;
+		this.cap = cap;
+		this.citta = citta;
+		this.prov = prov;
+		this.listaCartaCredito = listaCartaCredito;
 	}
 
 	public int getId() {
@@ -56,14 +109,69 @@ public class User {
 		this.profileType = profileType;
 	}
 
+	public TipoUtente getLogtype() {
+		return logtype;
+	}
+
+	public void setLogtype(TipoUtente logtype) {
+		this.logtype = logtype;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getVia() {
+		return via;
+	}
+
+	public void setVia(String via) {
+		this.via = via;
+	}
+
+	public String getCap() {
+		return cap;
+	}
+
+	public void setCap(String cap) {
+		this.cap = cap;
+	}
+
+	public String getCitta() {
+		return citta;
+	}
+
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+
+	public String getProv() {
+		return prov;
+	}
+
+	public void setProv(String prov) {
+		this.prov = prov;
+	}
+
+	public List<CartaCredito> getListaCartaCredito() {
+		return listaCartaCredito;
+	}
+
+	public void setListaCartaCredito(List<CartaCredito> listaCartaCredito) {
+		this.listaCartaCredito = listaCartaCredito;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + ", profileType=" + profileType + "]";
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", profileType=" + profileType
+				+ ", logtype=" + logtype + ", tel=" + tel + ", via=" + via + ", cap=" + cap + ", citta=" + citta
+				+ ", prov=" + prov + ", listaCartaCredito=" + listaCartaCredito + "]";
 	}
 	
-	
-	
-	
-	
-
 }
+	
+	
