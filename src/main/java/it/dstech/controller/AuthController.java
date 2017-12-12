e5406d3f58ee0250f68690100ccbf9cf48a7a13c
simone <s.sorcinelli@gmail.com>
2017-12-12 15:18:19 +0100

    inserito metodo logout

--- src/main/java/it/dstech/controller/AuthController.java
+++ src/main/java/it/dstech/controller/AuthController.java
@@ -1,13 +1,22 @@
 package it.dstech.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.dstech.model.User;
@@ -45,5 +54,14 @@ public class AuthController {
 	@DeleteMapping("/delete/{id}")
 	public void deleteUser(@PathVariable int id) {
 		userService.deleteUser(id);
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value= "/logoutApp", method = RequestMethod.OPTIONS)
+	public void logoutPage(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
 	}
 }
